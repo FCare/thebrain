6e94b449d56bfb6b6626688568429e6b64968612
FCare <francois.caron.perso@gmail.com>
2026-06-02 09:26:36 +0200

    Add thinking model

--- start.sh
+++ start.sh
@@ -13,7 +13,25 @@
     --min-p 0.0 \
     --flash-attn on \
     --presence-penalty 1.5 \
-    --ctx-size 49152 \
+    --ctx-size 131072 \
+    -ctk turbo4 \
+    -ctv turbo4 \
+    -fa 1
+
+./llama-cpp-turboquant/llama-server \
+    -hf unsloth/Qwen3-4B-Thinking-2507 \
+    --n-gpu-layers 99 \
+    --jinja \
+    --top-p 0.8 \
+    --top-k 20 \
+    --host 0.0.0.0 \
+    --port 9000 \
+    --temp 0.7 \
+    --parallel 6\
+    --min-p 0.0 \
+    --flash-attn on \
+    --presence-penalty 1.5 \
+    --ctx-size 131072 \
     -ctk turbo4 \
     -ctv turbo4 \
     -fa 1
